412d89ef45a4678cbf02455438305ab6b29ad393
Kevin Huaynates <6992@holbertonstudents.com>
2024-03-13 00:42:35 -0500

    1

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 
-$(document).ready(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Derechos de autor - Holberton School</p>');
 });
+
